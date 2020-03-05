f4bf91b5af15802a55c5d63378b4ee57f784c3b3
Chris <mrglass1@users.noreply.github.com>
2020-03-05 19:49:45 +1100

    created caching

--- cachematrix.R
+++ cachematrix.R
@@ -3,10 +3,13 @@
 
 ## This function will accept a matrix object and cache the inverse of it for future use
 
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
+makeCacheMatrix <- function(input_matrix = matrix()) {
+  # define sub functions
+  m <- NULL
+  set <- function(set_matrix_value) {
+    input_matrix <<- set_matrix_value #store in cache
+    m <<- NULL                        #store in cache
+  }
 
 ## This function will compute the inverse of a given matrix returned by "makecacheMatrix" above.
 ## If the inverse has not been solved then it stores it in a cache for future and faster retrieval
